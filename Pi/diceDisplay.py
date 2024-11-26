5f28a470b4271a45c58330dcc789db99cb6d3c65
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-11-25 18:19:55 -0600

    Update diceDisplay.py

--- Pi/diceDisplay.py
+++ Pi/diceDisplay.py
@@ -1,5 +1,6 @@
 '''
 Display Dice
+pygame sound reference: https://www.pygame.org/docs/ref/mixer.html
 '''
 
 from Display import *
@@ -7,34 +8,40 @@ import time
 from gpiozero import Button
 import random
 import signal
+from pygame import mixer
 
+mixer.init()
+ratchet = mixer.Sound("ratchet.wav")
 button = Button(18, pull_up=True)
+prevNumber = 10
 
 def randomNumber():
-    numbers = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
-    return numbers[random.randint(0, 9)]()
+    global prevNumber
+    leftDisplay = Display(LED(27, initial_value=True), LED(22, initial_value=True), LED(10, initial_value=True), LED(17, initial_value=True), LED(9, initial_value=True), LED(11, initial_value=True), bottomRight = LED(5, initial_value=True))
+    # rightDisplay = Display(LED(14, initial_value=True), LED(15, initial_value=True), LED(18, initial_value=True), LED(23, initial_value=True), LED(24, initial_value=True), LED(25, initial_value=True), LED(8, initial_value=True))
+    numbers = [leftDisplay.Zero, leftDisplay.One, leftDisplay.Two, leftDisplay.Three, leftDisplay.Four, leftDisplay.Five, leftDisplay.Six, leftDisplay.Seven, leftDisplay.Eight, leftDisplay.Nine]    
 
-def Roll():
+    number = random.randint(0, 9)
+    while number == prevNumber:
+        number = random.randint(0, 9)
+    prevNumber = number
+    return numbers[number]()
+
+while True:
+    while not button.is_active:
+        pass
     secs = random.uniform(.5, 8.0)
     amount = random.randint(3, 40)
     interval = secs / amount
-
     print(f'secs: {secs}\namount: {amount}\ninterval: {interval}\n')
-
     x = 0
-    while x < amount:
-        print(x)
+    while x < amount:   
+        ratchet.play()     
+        ratchet.fadeout(150)
         print(f'{randomNumber()}')
         time.sleep(interval)
         x += 1
         
-    randomNumber()
-    
-    time.sleep(.025)
-
-
-Clear()
-
-button.when_activated = Roll
-
-signal.pause()
+    ratchet.play()
+    ratchet.fadeout(150)
+    print(f'{randomNumber()}')
