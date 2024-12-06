53de7979689152eadf42c3897767225778d840dd
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-12-06 07:00:26 -0600

    Updated camera config

--- Pi/TrueRandom/cameraHandler.py
+++ Pi/TrueRandom/cameraHandler.py
@@ -2,9 +2,11 @@ from picamera2 import Picamera2, Preview
 
 class cameraHandler:
     def __init__(self, preview=True):
+        Picamera2.set_logging(Picamera2.ERROR)
         self.camera = Picamera2()
         self.preview = preview
-        self.camera.config(create_preview_configuration())
+        self.camera.configure(self.camera.create_preview_configuration())
+        self.camera.set_controls({"AwbEnable": True, "Contrast": 0.4, "Saturation" : 0.75, "ExposureTime": 5000})
         if (preview):
             self.camera.start_preview(Preview.QTGL)
         self.camera.start()
